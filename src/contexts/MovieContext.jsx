2d1fb231f74f61fdfd4a19d89bdd4a35e079c16f
swagnik10 <swagnikghosh07@gmail.com>
2026-02-20 19:48:07 +0530

    A small change

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect, use } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const MovieContext = createContext();
 
